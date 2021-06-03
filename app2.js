a23ec758c66f3a4b725d747e1f33888f0be62824
appel1611 <65170234+appel1611@users.noreply.github.com>
2021-06-03 08:05:16 -0300

    adicionado click to complete
    Adicionado a função de mudar o estilo quando clica na tarefa e criação da span com a lixeira para remover

--- app2.js
+++ app2.js
@@ -2,6 +2,7 @@ const input = document.getElementById("input");
 const btn = document.getElementById("btn");
 let todoList = document.getElementById("todoList");
 
+
 let tarefas = [];
 
 function addTodo(){
@@ -14,6 +15,18 @@ function addToList(){
     for(item of tarefas){
         todo.innerHTML = item;
         todoList.appendChild(todo);
+    }
+    if(todo){
+        todo.addEventListener("click", ()=>{
+            let addTrash =  document.createElement("span");
+            todo.classList.toggle("complete");
+            if(todo.classList.contains("complete")){
+                todo.appendChild(addTrash);
+                addTrash.innerHTML = `<i class="far fa-trash-alt"></i>`;
+                addTrash.style.color = "red"; 
+                console.log(todo);
+            };
+         })
     }
 }
 

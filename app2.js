0fb5804e2d9fcaff07e1949a32e4fba980fbce6a
appel1611 <65170234+appel1611@users.noreply.github.com>
2021-06-04 08:03:48 -0300

    Atualização
    Adicionado função de foco no input depois de adicionar tarefas e um alert caso o input esteja vazio.

--- app2.js
+++ app2.js
@@ -5,6 +5,7 @@ let todoList = document.getElementById("todoList");
 
 let tarefas = [];
 
+
 function addTodo(){
     let newTodo = input.value;
     tarefas.push(newTodo);
@@ -15,24 +16,32 @@ function addToList(){
     for(item of tarefas){
         todo.innerHTML = item;
         todoList.appendChild(todo);
+        
     }
     if(todo){
         todo.addEventListener("click", ()=>{
             let addTrash =  document.createElement("span");
+            
+            todo.appendChild(addTrash);
+            addTrash.innerHTML = `<i class=" delete far fa-trash-alt"></i>`;
             todo.classList.toggle("complete");
-            if(todo.classList.contains("complete")){
-                todo.appendChild(addTrash);
-                addTrash.innerHTML = `<i class="far fa-trash-alt"></i>`;
-                addTrash.style.color = "red"; 
-                console.log(todo);
-            };
-         })
+            addTrash.style.color = "red"; 
+            console.log(todo);
+        })
     }
+    
 }
 
+
 btn.addEventListener("click", function(){
-    addTodo();
-    console.log(tarefas);
-    input.value = "";
-    addToList();
+    if(input.value == "" || input.value == null){
+        alert("Insira uma tarefa!");
+        input.focus();
+    }else{
+        addTodo();
+        input.value = "";
+        input.focus();
+        addToList();    
+    }
+    
 })
